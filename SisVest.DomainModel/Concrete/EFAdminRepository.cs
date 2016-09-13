d2d0e75860bbe8942c14ff353bef51dd8275fc98
Ricardo Humberto Sa <r.humberto.sa@gmail.com>
2016-09-13 11:27:01 -0300

    Implementado AdminRepository

--- SisVest.DomainModel/Concrete/EFAdminRepository.cs
+++ SisVest.DomainModel/Concrete/EFAdminRepository.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 using SisVest.DomainModel.Entities;
@@ -17,33 +18,50 @@ namespace SisVest.DomainModel.Concrete
             vestContext = context;
         }
 
-        public IQueryable<Admin> admins
-        {
-            get
-            {
-                return vestContext.Admins.AsQueryable();
-            }
-        }
+        public IQueryable<Admin> admins => vestContext.Admins.AsQueryable();
 
         public void Alterar(Admin admin)
         {
-            throw new NotImplementedException();
+            var result = from a in admins
+                         where (a.sLogin.ToUpper().Equals(admin.sLogin) || a.sEmail.ToUpper().Equals(admin.sEmail)) 
+                         &&!a.iAdminId.Equals(a.iAdminId)
+                         select a;
+
+            if (result.Count() > 0)
+                throw new InvalidOperationException("Já existe um Administrador cadastrado com este Login ou Email.");
+
+            vestContext.SaveChanges();
         }
 
         public void Exclir(int iCandidatoID)
         {
-            throw new NotImplementedException();
+            var result = from a in admins
+                where a.iAdminId.Equals(iCandidatoID)
+                select a;
+
+            if (result.Count() == 0)
+                throw new InvalidOperationException("Administrador não localizado no repositório.");
+            
+            vestContext.Admins.Remove(result.FirstOrDefault());
+            vestContext.SaveChanges();
         }
 
         public void Inserir(Admin admin)
         {
+            var result = from a in admins
+                         where a.sLogin.ToUpper().Equals(admin.sLogin) || a.sEmail.ToUpper().Equals(admin.sEmail)
+                         select a;
+
+            if (result.Count() > 0)
+                throw new InvalidOperationException("Administrador já cadastrado com este Login ou Email.");
+
             vestContext.Admins.Add(admin);
             vestContext.SaveChanges();
         }
 
         public Admin Retornar(int id)
         {
-            throw new NotImplementedException();
+            return vestContext.Admins.Where(a => a.iAdminId == id).FirstOrDefault();
         }
     }
 }
--- SisVest.Test/Repositories/AdminRepositoriesTest.cs
+++ SisVest.Test/Repositories/AdminRepositoriesTest.cs
@@ -12,7 +12,7 @@ namespace SisVest.Test.Repository
     {
         private IAdimRepository adminRepository;
         private VestContext vestContext = new VestContext();
-        private Admin adminInserir;
+        public Admin adminInserir;
 
         [TestInitialize]
         public void InicializarTeste()
@@ -24,7 +24,7 @@ namespace SisVest.Test.Repository
                 sEmail = "r.humberto.sa@gmail.com",
                 sLogin = "rhumbertosa",
                 sNomeTratamento = "Ricardo",
-                sSenha = "dmt8017"
+                sSenha = "123456"
             });
         }
 
@@ -64,6 +64,7 @@ namespace SisVest.Test.Repository
             //Ação
 
             adminRepository.Inserir(adminInserir);
+            vestContext.SaveChanges();
 
             var retorno = (from a in adminRepository.admins
                            where a.sLogin.Equals(adminInserir.sLogin)
@@ -74,21 +75,264 @@ namespace SisVest.Test.Repository
 
         }
 
-        [TestCleanup]
-        public void LimparCenario()
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Pode_Inserir_Admin_Com_Mesmo_Email_Test()
         {
-            var adminExclusao = (from a in vestContext.Admins
-                                 where a.sLogin.Equals(adminInserir.sLogin)
-                                 select a);
-
-            if (adminExclusao.Count() > 0)
+            //Ambiente
+            var adminInserir = (new Admin()
             {
-                vestContext.Admins.Remove(adminExclusao.FirstOrDefault());
+                sEmail = "r.humberto.sa@gmail.com",
+                sLogin = "rhumbertosa",
+                sNomeTratamento = "Ricardo",
+                sSenha = "dmt8017"
+            });
 
-                vestContext.SaveChanges();
-            }
+            var adminInserir2 = (new Admin()
+            {
+                sEmail = adminInserir.sEmail,
+                sLogin = "rhumbertosa",
+                sNomeTratamento = "Ricardo",
+                sSenha = "dmt8017"
+            });
+
+            adminRepository.Inserir(adminInserir);
+            //Ação            
+            adminRepository.Inserir(adminInserir2);
+
+
+            //Assertivas            
+
 
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Pode_Inserir_Admin_Com_Mesmo_Login_Test()
+        {
+            //Ambiente
+            var adminInserir = (new Admin()
+            {
+                sEmail = "r.humberto.sa@gmail.com",
+                sLogin = "rhumbertosa",
+                sNomeTratamento = "Ricardo",
+                sSenha = "dmt8017"
+            });
+
+            var adminInserir2 = (new Admin()
+            {
+                sEmail = "teste@gmail.com.br",
+                sLogin = adminInserir.sLogin,
+                sNomeTratamento = "Ricardo",
+                sSenha = "dmt8017"
+            });
+
+            adminRepository.Inserir(adminInserir);
+            //Ação
+
+            adminRepository.Inserir(adminInserir2);
+
+            //Assertivas            
+
+        }
+
+
+
+
+        [TestMethod]
+        public void Pode_Altera_Test()
+        {
+            //Ambiente    
+            var adminInserir = (new Admin()
+            {
+                sEmail = "r.humberto.sa@gmail.com",
+                sLogin = "rhumbertosa",
+                sNomeTratamento = "Ricardo",
+                sSenha = "dmt8017"
+            });
+
+            adminRepository.Inserir(adminInserir);
+
+            var emailEsperado = adminInserir.sEmail;
+            var loginEsperado = adminInserir.sLogin;
+            var nomeTratamentoEsperado = adminInserir.sNomeTratamento;
+            var senhaEsperada = adminInserir.sSenha;
+
+
+            var adminAlterar = (from a in adminRepository.admins
+                                where a.iAdminId == adminInserir.iAdminId
+                                select a).FirstOrDefault();
+
+            adminAlterar.sEmail = "miguel@gmail.com";
+            adminAlterar.sLogin = "Miguelsa";
+            adminAlterar.sNomeTratamento = "Miguel";
+            adminAlterar.sSenha = "45644";
+
+            //Ação
+            adminRepository.Alterar(adminAlterar);
+
+            var retorno = (from a in adminRepository.admins
+                          where a.iAdminId.Equals(adminInserir.iAdminId)
+                          select a).FirstOrDefault();
+            //Assertivas
+            Assert.AreEqual(retorno.iAdminId, adminAlterar.iAdminId);
+            Assert.AreNotEqual(emailEsperado, adminAlterar.sEmail);
+            Assert.AreNotEqual(loginEsperado, adminAlterar.sLogin);
+            Assert.AreNotEqual(nomeTratamentoEsperado, adminAlterar.sNomeTratamento);
+            Assert.AreNotEqual(senhaEsperada, adminAlterar.sSenha);
+
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Nao_Alterar_Admin_Com_Mesmo_Email_Test()
+        {
+            //Ambiente
+            var adminInserir = (new Admin()
+            {
+                sEmail = "r.humberto.sa@gmail.com",
+                sLogin = "rhumbertosa",
+                sNomeTratamento = "Ricardo",
+                sSenha = "dmt8017"
+            });
+
+            adminRepository.Inserir(adminInserir);
+
+            var adminAlterar = (from a in adminRepository.admins
+                                where a.iAdminId == adminInserir.iAdminId
+                                select a).FirstOrDefault();
+
+            adminAlterar.sEmail = adminInserir.sEmail;
+            adminAlterar.sLogin = "Miguelsa";
+            adminAlterar.sNomeTratamento = "Miguel";
+            adminAlterar.sSenha = "45644";
+
+            //Ação
+            adminRepository.Alterar(adminAlterar);
+
+
+            //Assertivas            
+
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Nao_Alterar_Admin_Com_Mesmo_Login_Test()
+        {
+            //Ambiente
+            var adminInserir = (new Admin()
+            {
+                sEmail = "r.humberto.sa@gmail.com",
+                sLogin = "rhumbertosa",
+                sNomeTratamento = "Ricardo",
+                sSenha = "dmt8017"
+            });
+
+            adminRepository.Inserir(adminInserir);
+
+            var adminAlterar = (from a in adminRepository.admins
+                                where a.iAdminId == adminInserir.iAdminId
+                                select a).FirstOrDefault();
+
+            adminAlterar.sEmail = "miguel@gmail.com";
+            adminAlterar.sLogin = adminInserir.sLogin;
+            adminAlterar.sNomeTratamento = "Miguel";
+            adminAlterar.sSenha = "45644";
+
+            //Ação
+            adminRepository.Alterar(adminAlterar);
+
+            //Assertivas            
+
+        }
+
+        [TestMethod]
+        public void Pode_Excluir_Test()
+        {
+            //Ambiente
+            var adminInserir = (new Admin()
+            {
+                sEmail = "r.humberto.sa@gmail.com",
+                sLogin = "rhumbertosa",
+                sNomeTratamento = "Ricardo",
+                sSenha = "dmt8017"
+            });
+
+            adminRepository.Inserir(adminInserir);
+            
+
+            //Ação
+            adminRepository.Exclir(adminInserir.iAdminId);
+
+            //Assertivas            
+            var result = from a in vestContext.Admins
+                         where a.iAdminId.Equals(adminInserir.iAdminId)
+                                    select a;
+
+            Assert.AreEqual(0, result.Count());
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Nao_Pode_Excluir_Dados_Que_Nao_Existe_Test()
+        {
+            //Ambiente
+          
+            
+            //Ação
+            adminRepository.Exclir(10050);
+
+            //Assertivas            
+            
+        }
+
+
+
+        [TestMethod]
+        public void Retornar_Dados_Por_ID_Test()
+        {
+            //Ambiente
+            var adminInserir = (new Admin()
+            {
+                sEmail = "r.humberto.sa@gmail.com",
+                sLogin = "rhumbertosa",
+                sNomeTratamento = "Ricardo",
+                sSenha = "dmt8017"
+            });
+
+            adminRepository.Inserir(adminInserir);
+
+            //Ação
+            
+            var result = adminRepository.Retornar(adminInserir.iAdminId);
+
+            //Assertivas            
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Admin));
+            Assert.AreEqual(adminInserir, result);
+
+        }
+
+
+        //[TestCleanup]
+        //public void LimparCenario()
+        //{
+        //    var adminsParaRemover = from a in vestContext.Admins
+        //                            select a;
+
+        //    foreach (var admin in adminsParaRemover)
+        //    {
+        //        vestContext.Admins.Remove(admin);
+
+        //    }
+
+        //    vestContext.SaveChanges();
+        //}
+
+
     }
 }
 
